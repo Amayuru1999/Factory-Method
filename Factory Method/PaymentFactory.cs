76a7b36b2fdcc5d73223404bc9b0e9d680e19bc0
Amayuru1999 <amarasingheau@gmail.com>
2024-09-03 11:09:17 +0530

    feat: implemented PaymentFactory.cs method

---
+++ Factory Method/PaymentFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Factory_Method.Enums;
+using Factory_Method.Implementations;
+
+namespace Factory_Method
+{
+    public class PaymentFactory
+    {
+        public static IPayment Create(PaymentMethod paymentMethod)
+        {
+            switch (paymentMethod)
+            {
+                case PaymentMethod.CreditCard:
+                    return new CreditCardPayment();
+                
+                case PaymentMethod.PayPal:
+                    return new PayPalPayment();
+                
+                case PaymentMethod.GooglePay:
+                    return new GooglePayPayment();
+                
+                default:
+                    throw new NotSupportedException(
+                        $"{paymentMethod} is not supported by the system."
+                        );
+                
+            }
+            
+        }
+    }
+}
--- Factory Method/Program.cs
+++ Factory Method/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Factory_Method
+﻿using Factory_Method.Enums;
+using Factory_Method.Implementations;
+using System;
+
+
+namespace Factory_Method
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            IPayment payment = new PaymentFactory().Create(PaymentMethod.CreditCard);
+            payment.Pay(1000.00);
         }
     }
 }
