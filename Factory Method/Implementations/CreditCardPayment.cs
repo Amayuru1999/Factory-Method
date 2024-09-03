27713d589b4ccd0f349459d164cd436fed43fa8f
Amayuru1999 <amarasingheau@gmail.com>
2024-09-03 10:49:35 +0530

    feat: implemented CreditCardPayment.cs method

---
+++ Factory Method/Implementations/CreditCardPayment.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Factory_Method.Implementations
+{
+    public class CreditCardPayment : IPayment
+    {
+        public void Pay(double amount)
+        {
+            Console.WriteLine($"Successfully paid {amount} with credit card.");
+        }
+    }
+}
