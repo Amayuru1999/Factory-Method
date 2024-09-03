d1648366579cf4c81a024e16dc2c0a5829456f81
Amayuru1999 <amarasingheau@gmail.com>
2024-09-03 10:51:57 +0530

    feat: implemented GooglePayPayment.cs method

---
+++ Factory Method/Implementations/GooglePayPayment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Factory_Method.Implementations
+{
+    public class GooglePayPayment : IPayment
+    {
+        public void Pay(double amount)
+        {
+            Console.WriteLine($"Successfully paid {amount} with Google Pay.");
+        }
+    }
+}
