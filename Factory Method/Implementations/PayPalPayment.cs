469233a7afa10e64ca35cfc08736b24231b1f0c1
Amayuru1999 <amarasingheau@gmail.com>
2024-09-03 10:55:31 +0530

    feat: implemented PayPalPayment.cs method

---
+++ Factory Method/Implementations/PayPalPayment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Factory_Method.Implementations
+{
+    public class PayPalPayment : IPayment
+    {
+        public void Pay(double amount)
+        {
+            Console.WriteLine($"Successfully paid {amount} with PayPal.");
+        }
+    }
+}
